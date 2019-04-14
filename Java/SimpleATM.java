7c44c744ab3d26a6afc3ecfdc8a2720ab6eb312a
RafliArsya <47909089+RafliArsya@users.noreply.github.com>
2019-04-14 21:34:08 +0700

    Update SimpleATM.java

--- Java/SimpleATM.java
+++ Java/SimpleATM.java
@@ -15,44 +15,62 @@ public class SimpleATM {
     /**
      * @param args the command line arguments
      */
+    public static double deposits(double a, double b){
+        double mybalance=a+b;
+        return mybalance;
+    }
+    public static double withdraws(double a, double b){
+        double mybalance=b-a;
+        return mybalance;
+    }
+    public static void check(double a){
+        String info3="Tabungan anda = Rp";
+        System.out.print(info3+a+",-\n");
+    }
+    
     public static void main(String[] args) {
         // TODO code application logic here
         double balance=0, withdraw, Deposit;
-        String salam="Selamat Datang di Banktut", info0="Pilih Layanan\n[W]ithdraw\n[D]eposit\n[C]heck\n", info1="Masukan Nominal = \n", info2="Uang tidak mencukupi\n", info3="Tabungan anda = Rp", infosalah="Pilihan salah!";
-        
+        boolean loop=true;
+        String salam="Selamat Datang di Banktut", info0="Pilih Layanan\n[W]ithdraw\n[D]eposit\n[C]heck\n[E]xit", info1="Masukan Nominal = \n", info2="Uang tidak mencukupi\n", infosalah="Pilihan salah!";
         Scanner input = new Scanner(System.in);
         System.out.println(salam+"\n");
         System.out.println(info0);
-        String pilihan;
-        
-        do{
+        String pilihan="E";
+        while(loop==true){
             pilihan = input.next();
             switch(pilihan){
                 case ("D"):
                 case ("d"):
                     System.out.println(info1);
                     Deposit = input.nextDouble();
-                    balance = balance + Deposit;
+                    balance = deposits(balance, Deposit);
                     break;
                 case ("W"):
                 case ("w"):
                     System.out.println(info1);
                     withdraw = input.nextDouble();
-                    if (withdraw > balance){
+                    if(withdraw>balance){
                         System.out.println(info2);
-                    }else{
-                        balance = balance - withdraw;
+                        break;
                     }
+                    balance=withdraws(withdraw, balance);
                     break;
                 case ("C"):
                 case ("c"):
-                    System.out.print(info3+balance+",-\n");
+                    check(balance);
+                    break;
+                case ("E"):
+                case ("e"):
+                    loop=false;
                     break;
                 default :
                     System.out.println(infosalah);
                     break;
             }
-            System.out.println(info0);
-        }while((!pilihan.equals("e")) || (!pilihan.equals("E")));
+            if(loop){
+                System.out.println(info0);
+            }
+        }
     }
 }
