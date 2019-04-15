1f79c1d96e1ac928af2a2f96b1b430ef1248062f
RafliArsya <47909089+RafliArsya@users.noreply.github.com>
2019-04-15 14:13:51 +0700

    More Variable Types

--- C#/Limas_Segitiga.cs
+++ C#/Limas_Segitiga.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,9 +13,10 @@ namespace Limas_Segitiga
             float luas, alas, tinggi, tinggi_, volume;
             const float set = 0.5f, sep = 0.33f;
             string nama = "YUSUF RAFLI B", nim = "21120118130101";
-            int shift=4, Kelompok=51;
+            int shift = 4;//, Kelompok=51;
+            char[] Kelompok = { 'L', 'I' };
 
-            Console.Write("Nama : {0}, NIM : {1}\nKelompok : {2}, Shift : {3}\n", nama, nim, Kelompok, shift);
+            Console.Write("Nama : {0}, NIM : {1}\nKelompok : {2}{3}, Shift : {4}\n", nama, nim, Kelompok[0], Kelompok[1], shift);
             Console.Write("Masukkan alas : ");
 
             while (!float.TryParse(Console.ReadLine(), out alas))
@@ -35,7 +36,7 @@ namespace Limas_Segitiga
             
             luas = alas * tinggi * set;
             volume = sep * luas * tinggi_;
-            Console.WriteLine("Luas Limas Segitiga = 1/3 * luas alas * tinggi limas\nLuas Limas Segitiga = 1/3 * {0} * {1} = {2}", +luas, tinggi_, volume);
+            Console.WriteLine("Luas Limas Segitiga = 1/3 * luas alas * tinggi limas\nLuas Limas Segitiga = 1/3 * {0} * {1} = {2}", luas, tinggi_, volume);
 
             Console.ReadKey();
         }
