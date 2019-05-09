1749b856e0a2bbe313f088e1e12715f9be1428f5
RafliArsya <47909089+RafliArsya@users.noreply.github.com>
2019-05-10 04:42:56 +0700

    Tugas Modul 5

---
+++ C#/Tugas_Modul5/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tugas_Kelompok51
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int count;
+            Console.WriteLine("Berapa objek?");
+            while (!int.TryParse(Console.ReadLine(), out count))
+            {
+                Console.WriteLine("Format Masukan Salah!\nMasukan kembali banyak objek! ");
+            }
+            smartphone[] hp = new smartphone[count];
+            for (int i = 0; i < count; i++)
+            {
+                Console.WriteLine("Merk objek ke {0} :", i+1);
+                string brand = Console.ReadLine();
+                Console.WriteLine("Pembeli objek ke {0} :", i + 1);
+                string owner = Console.ReadLine();
+                Console.WriteLine("Garansi objek ke {0} :", i + 1);
+                string warranty = Console.ReadLine();
+                Console.WriteLine("Harga objek ke {0}", i + 1);
+                double price = double.Parse(Console.ReadLine());
+                Console.WriteLine("Status objek ke {0}", i + 1);
+                string status = Console.ReadLine();
+                hp[i] = new smartphone(brand, owner, warranty, price, status);
+            }
+            for (int i = 0; i < count; i++)
+            {
+                hp[i].print("Smartphone bermerk" + hp[i].merk_() + ", Pemilik " + hp[i].owner_() + ", Garansi " + hp[i].warranty_() + ", dengan harga " + hp[i].price_() + ", kondisi/status barang " + hp[i].status_() + ".");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ C#/Tugas_Modul5/smartphone.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tugas_Kelompok51
+{
+    class smartphone
+    {
+        private string merk, owner, warranty, status;
+        private double price;
+        public smartphone() {}
+        public smartphone(string merk, string owner, string warranty, double price, string status) {
+            this._merk(merk);
+            this._owner(owner);
+            this._warranty(warranty);
+            this._price(price);
+            this._status(status);
+        }
+        public string merk_()
+        {
+            return this.merk;
+        }
+        public string owner_()
+        {
+            return this.owner;
+        }
+        public string warranty_()
+        {
+            return this.warranty;
+        }
+        public Double price_()
+        {
+            return this.price;
+        }
+        public string status_()
+        {
+            return this.status;
+        }
+        public void _merk(string merk)
+        {
+            this.merk = merk;
+        }
+        public void _owner(string owner)
+        {
+            this.owner = owner;
+        }
+        public void _warranty(string warranty)
+        {
+            this.warranty = warranty;
+        }
+        public void _price(double price)
+        {
+            this.price = price;
+        }
+        public void _status(string status)
+        {
+            this.status = status;
+        }
+        public void print(string a)
+        {
+            Console.WriteLine(a);
+        }
+    }
+}
